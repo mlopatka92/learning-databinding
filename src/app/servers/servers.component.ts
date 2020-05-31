36e1ad848ffb5332ec064f24f869fd5d0876469b
marcin.lopatka <mlopatka.krk@gmail.com>
2020-05-31 08:46:52 +0200

    Add databinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,21 @@ import {Component} from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  constructor() {
+    // This is javascript native function
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Named as: ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
+  }
 }
